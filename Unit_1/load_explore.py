a1b18287cb5afc2b7b3f305024d56086f54f8765
Lahad Mbacke <lahadmbacke00@gmail.com>
2025-01-12 15:34:23 +0100

    finish load and exolore

--- Unit_1/load_explore.py
+++ Unit_1/load_explore.py
@@ -1,1 +1,42 @@
 #%%
+!pip install datasets[audio] gradio -q
+# %%
+from datasets import load_dataset
+
+minds = load_dataset("PolyAI/minds14", name="en-AU", split="train")
+minds
+#%%
+example = minds[0]
+example
+
+# The intent_class is a classification category of the audio recording. To convert this number into a meaningful string, we can use the int2str() method
+id2label = minds.features["intent_class"].int2str
+id2label(example["intent_class"])
+
+#%%
+import gradio as gr
+
+def generate_audio():
+    example = minds.shuffle()[0]
+    audio = example["audio"]
+    return(
+        audio["sampling_rate"],
+        audio["array"]
+    ),id2label(example["intent_class"])
+
+with gr.Blocks() as demo:
+    with gr.Column():
+        for _ in range(4):
+            audio, label = generate_audio()
+            output = gr.Audio(audio, label=label)
+
+demo.launch(debug=True)
+# %%
+import matplotlib.pyplot as plt
+import librosa.display
+
+plt.figure().set_figwidth(12)
+example = minds.shuffle()[0]
+audio = example["audio"]
+librosa.display.waveshow(audio["array"], sr=audio["sampling_rate"])
+# %%
